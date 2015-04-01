4652f3f583f896599e79c39a37d49b9147f91cde
conorcurlett <conorcurlett@gmail.com>
2015-04-01 17:16:42 +1100

    Added authentication with Connection Testing to complete Story #2

--- src/main/java/com/wsl/modules/stripe/StripeConnector.java
+++ src/main/java/com/wsl/modules/stripe/StripeConnector.java
@@ -5,16 +5,20 @@
 
 package com.wsl.modules.stripe;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.mule.api.annotations.ConnectionStrategy;
 import org.mule.api.annotations.Connector;
 import org.mule.api.annotations.Configurable;
-
 import org.mule.api.annotations.Processor;
 import org.mule.api.annotations.oauth.OAuthProtected;
 import org.mule.api.annotations.ReconnectOn;
-
 import org.mule.api.annotations.param.Default;
+import org.mule.api.annotations.param.Optional;
 
+import com.stripe.model.Customer;
+import com.stripe.model.CustomerCollection;
 import com.wsl.modules.stripe.strategy.ConnectorConnectionStrategy;
 
 /**
@@ -36,22 +40,28 @@ public class StripeConnector {
     ConnectorConnectionStrategy connectionStrategy;
 
     /**
-     * Custom processor
+     * List all Customers
      *
-     * {@sample.xml ../../../doc/stripe-connector.xml.sample stripe:greet}
+     * {@sample.xml ../../../doc/stripe-connector.xml.sample stripe:list-all-customers}
      *
-     * @param friend Content to be processed
-     * @return Some string
+     * @param limit A limit on the number of records to fetch in a batch
+     * @return CustomerCollection of Customers found
      * @throws Exception Comment for Exception     
      */
     @Processor
-    @OAuthProtected
     @ReconnectOn(exceptions = { Exception.class })
-    public String greet(@Default("friend") String friend) throws Exception {
-        /*
-         * MESSAGE PROCESSOR CODE GOES HERE
-         */
-        return greeting + " " + friend + ". " + connectionStrategy.getReplay();
+    public CustomerCollection listAllCustomers(@Default("0") int limit /*@Optional Customer startingAfter, @Optional Customer endingBefore*/) throws Exception {
+    	Map<String, Object> customerParams = new HashMap<String, Object>();
+    	if (limit > 0){
+    		customerParams.put("limit", limit);
+    	}
+    	/*if (startingAfter != null) {
+    		customerParams.put("starting_after", startingAfter);    			
+    	}
+    	if (endingBefore != null) {
+    		customerParams.put("ending_before", endingBefore);
+    	}*/
+    	return Customer.all(customerParams);
     }
 
     /**
--- src/main/java/com/wsl/modules/stripe/strategy/ConnectorConnectionStrategy.java
+++ src/main/java/com/wsl/modules/stripe/strategy/ConnectorConnectionStrategy.java
@@ -5,122 +5,127 @@
 
 package com.wsl.modules.stripe.strategy;
 
-import org.mule.api.annotations.Configurable;
+import org.mule.api.annotations.components.ConnectionManagement;
+import org.mule.api.annotations.TestConnectivity;
 import org.mule.api.annotations.Connect;
+import org.mule.api.annotations.ValidateConnection;
 import org.mule.api.annotations.ConnectionIdentifier;
 import org.mule.api.annotations.Disconnect;
-import org.mule.api.annotations.TestConnectivity;
-import org.mule.api.annotations.ValidateConnection;
-import org.mule.api.annotations.display.Password;
-import org.mule.api.annotations.oauth.OAuth2;
-import org.mule.api.annotations.oauth.OAuthAccessToken;
-import org.mule.api.annotations.oauth.OAuthConsumerKey;
-import org.mule.api.annotations.oauth.OAuthConsumerSecret;
 import org.mule.api.annotations.param.ConnectionKey;
+import org.mule.api.ConnectionException;
+import org.mule.api.ConnectionExceptionCode;
+import org.mule.api.annotations.display.Password;
+import org.mule.api.annotations.Configurable;
 import org.mule.api.annotations.param.Default;
 
-/**
- * OAuth2 Connection Strategy
- *
- * @author MuleSoft, Inc.
- */
-@OAuth2( configElementName = "config-type", friendlyName="OAuth2 Configuration", authorizationUrl = "https://api.myconnector.com/uas/oauth/authorize", 
-accessTokenUrl = "https://api.myconnector.com/uas/oauth/accessToken", 
-accessTokenRegex = "\"access_token\":\"([^&]+?)\"",
-expirationRegex = "\"expires_in\":([^&]+?),", 
-refreshTokenRegex = "\"refresh_token\":\"([^&]+?)\"" )
-public class ConnectorConnectionStrategy
-{
-    /**
-     * Configurable
-     */
-    @Configurable
-    @Default("How are you?")
-    private String replay;
+import com.stripe.Stripe;
+import com.stripe.exception.APIConnectionException;
+import com.stripe.exception.APIException;
+import com.stripe.exception.AuthenticationException;
+import com.stripe.exception.CardException;
+import com.stripe.exception.InvalidRequestException;
+import com.stripe.model.Balance;
+import com.stripe.model.Customer;
+import com.stripe.model.CustomerCollection;
 
-    /**
-     * The OAuth access token
-     */
-    @OAuthAccessToken
-    private String accessToken;
+/**
+ * Connection Management Strategy
+ *
+ * @author WhiteSky Labs
+ */
+@ConnectionManagement(configElementName = "config-type", friendlyName = "Connection Management type strategy")
+public class ConnectorConnectionStrategy {
+    
+    @Configurable
+    @Default("2015-03-24")
+    private String apiVersion;
     
     /**
-     * The OAuth consumer key
-     */
-    @Configurable
-    @OAuthConsumerKey
-    private String consumerKey;
-
-    /**
-     * The OAuth consumer secret
-     */
-    @Configurable
-    @OAuthConsumerSecret
-    private String consumerSecret;
-
-    /**
-     * Set strategy property
+     * Connect
      *
-     * @param replay my strategy property
+     * @param apiKey Your Stripe API Key
+     * @throws ConnectionException
      */
-    public void setReplay(String replay) {
-        this.replay = replay;
+    @Connect
+    @TestConnectivity
+    public void connect(@ConnectionKey String apiKey)
+        throws ConnectionException {
+        Stripe.apiKey = apiKey;
+        Stripe.apiVersion = this.apiVersion;
+        try {
+			Balance balance = Balance.retrieve();
+		} catch (AuthenticationException e) {
+			throw new ConnectionException(
+		             ConnectionExceptionCode.INCORRECT_CREDENTIALS, 
+		             e.getMessage(),
+		             "Your API Key is probably incorrect");
+		} catch (InvalidRequestException e) {
+			throw new ConnectionException(
+		             ConnectionExceptionCode.UNKNOWN, 
+		             e.getMessage(),
+		             "The request was invalid. Your API version might be incorrect.");
+		} catch (APIConnectionException e) {
+			throw new ConnectionException(
+		             ConnectionExceptionCode.CANNOT_REACH, 
+		             e.getMessage(),
+		             "Could not get a connection to Stripe. Check your API Version and Connectivity.");
+		} catch (CardException e) {
+			throw new ConnectionException(
+		             ConnectionExceptionCode.UNKNOWN, 
+		             e.getMessage(),
+		             "A Card Error shouldn't happen here. Check your API Version.");
+		} catch (APIException e) {
+			throw new ConnectionException(
+		             ConnectionExceptionCode.CANNOT_REACH, 
+		             e.getMessage(),
+		             "There was an issue with the API, check your API Version and Key");
+		}    	
+        //if we manage to get here, it means that the connection was 
+        // successful, hence, no need to return a boolean           
     }
 
     /**
-     * Get property
+     * Disconnect
      */
-    public String getReplay() {
-        return this.replay;
+    @Disconnect
+    public void disconnect() {
+        Stripe.apiKey = null;
     }
 
     /**
-     * Set accessToken
+     * Are we connected
+     */
+    @ValidateConnection
+    public boolean isConnected() {
+        if (Stripe.apiKey != null) {
+        	return true;
+        } else {
+        	return false;
+        }
+    }
+
+    /**
+     * The Connection Identifier
+     * For now, we are using the Stripe SDK Version, arbitrarily
+     */
+    @ConnectionIdentifier
+    public String connectionId() {
+        return Stripe.VERSION;
+    }
+
+    /**
+     * Set API Version
      *
-     * @param accessToken
-     *            The accessToken
+     * @param apiVersion The API Version 
      */
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
+    public void setApiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
     }
 
     /**
-     * Get accessToken
+     * Get configured API Version
      */
-    public String getAccessToken() {
-        return this.accessToken;
+    public String getApiVersion() {
+        return this.apiVersion;
     }
-
-    /**
-     * Set consumerKey
-     *
-     * @param consumerKey The consumerKey
-     */
-    public void setConsumerKey(String consumerKey) {
-        this.consumerKey = consumerKey;
-    }
-
-    /**
-     * Get consumerKey
-     */
-    public String getConsumerKey() {
-        return this.consumerKey;
-    }
-
-    /**
-     * Set consumerSecret
-     *
-     * @param consumerSecret The consumerSecret
-     */
-    public void setConsumerSecret(String consumerSecret) {
-        this.consumerSecret = consumerSecret;
-    }
-
-    /**
-     * Get consumerSecret
-     */
-    public String getConsumerSecret() {
-        return this.consumerSecret;
-    }
-
 }
