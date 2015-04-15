64ceda9d08904c77a653913b51c90b5850d35b29
conorcurlett <conorcurlett@gmail.com>
2015-04-15 16:01:54 +1000

    Added license headers

--- src/test/java/com/wsl/modules/stripe/StripeClientUtilsTest.java
+++ src/test/java/com/wsl/modules/stripe/StripeClientUtilsTest.java
@@ -1,3 +1,12 @@
+/**
+ *
+ * (c) 2015 WhiteSky Labs, Pty Ltd. This software is protected under international
+ * copyright law. All use of this software is subject to WhiteSky Labs' Master
+ * Subscription Agreement (or other Terms of Service) separately entered
+ * into between you and WhiteSky Labs. If such an agreement is not in
+ * place, you may not use the software.
+ */
+
 package com.wsl.modules.stripe;
 
 import static org.junit.Assert.assertEquals;
